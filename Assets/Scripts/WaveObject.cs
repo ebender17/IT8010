3a7e26269cd81783af494f5d7991be86e3ff4548
Emily Bender <bender.emily1@gmail.com>
2022-03-30 18:47:15 -0400

    Option to chose which axis objects move on

--- Assets/Scripts/WaveObject.cs
+++ Assets/Scripts/WaveObject.cs
@@ -2,6 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum AxisMovement
+{
+    xAxis, 
+    yAxis, 
+    zAxis
+}
+
 public class WaveObject : MonoBehaviour
 {
     private Vector3 _startingPosition;
@@ -11,6 +18,7 @@ public class WaveObject : MonoBehaviour
     [SerializeField] private int _frequency = 1;
     [SerializeField] private int _amplitude = 1;
     [SerializeField] private int _verticalShift = 0;
+    [SerializeField] AxisMovement _axisToMoveOn = AxisMovement.yAxis;
     [HideInInspector] public int Frequency { 
         get { return _frequency; } 
         private set { _frequency = value; } 
@@ -32,16 +40,39 @@ public class WaveObject : MonoBehaviour
     void Start()
     {
         _startingPosition = transform.position;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         _rawSineWave = (Mathf.Sin(Time.time * _frequency) * _amplitude) + _verticalShift;
-        _newPosition.y = (_rawSineWave + 1f) / 2f; //transform to 0 and 2 then divide by 2 to get value between 0 and 1
 
-        _newPosition.x = 0.0f;
-        _newPosition.z = 0.0f;
+        if(_axisToMoveOn == AxisMovement.xAxis)
+        {
+            _newPosition.x = (_rawSineWave + 1f) / 2f;
+
+            _newPosition.y = 0.0f;
+            _newPosition.z = 0.0f;
+
+        }
+        else if(_axisToMoveOn == AxisMovement.yAxis)
+        {
+            _newPosition.y = (_rawSineWave + 1f) / 2f; //transform to 0 and 2 then divide by 2 to get value between 0 and 1
+
+            _newPosition.x = 0.0f;
+            _newPosition.z = 0.0f;
+
+        }
+        else if(_axisToMoveOn == AxisMovement.zAxis)
+        {
+            _newPosition.z = (_rawSineWave + 1f) / 2f;
+
+            _newPosition.x = 0.0f;
+            _newPosition.y = 0.0f;
+        }
+        
 
         transform.position = _startingPosition + _newPosition;
 
